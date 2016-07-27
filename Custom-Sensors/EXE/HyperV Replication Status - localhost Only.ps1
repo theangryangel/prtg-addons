01c12f4156faadd1f76ddc804e3281ddf940dfc8
Karl <karl@theangryangel.co.uk>
2016-07-27 12:50:40 +0100

    Create HyperV Replication Status - localhost Only.ps1

---
+++ Custom-Sensors/EXE/HyperV Replication Status - localhost Only.ps1
@@ -0,0 +1,46 @@
+Import-Module Hyper-V
+ 
+$VMReplication = Get-VMReplication | select Name,Health,State
+
+$ErrMessage = " "
+$Errors = 0
+$Warning = 0
+$Critical = 0
+foreach ($Name in $VMReplication) {
+  if ($Name.Health -Contains "Normal" -and $Name.State -Contains "Replicating") {
+    $stat = 0
+  }
+  elseif ($Name.Health = "Warning") {
+    $stat = 1
+    $Warning = $Warning + 1
+  }
+  elseif ($Name.Health = "Critical") {
+    $stat = 1
+    $Critical = $Critical + 1
+  }
+
+  if ($stat -gt 0) {
+    $VM = $Name.Name
+    $Health = $Name.Health
+    $Status = $Name.State
+    $ErrMessage = $ErrMessage, $VM, " " , $Health , " (", $Status, ") "
+    $Errors = $Errors + 1
+  }
+}
+
+If ($Errors -eq 0) {
+  Write-Host 0, ":OK", "Replication is working fine"
+  Exit 0
+}
+elseif ($Critical -gt 0) {
+  Write-Host $Errors, ":", $ErrMessage 
+  Exit 4
+}
+elseif ($Warning -gt 0) {
+  Write-Host $Errors, ":", $ErrMessage
+  Exit 1 
+}
+else {
+  Write-Host $Errors, ":", $ErrMessage
+  Exit 2
+}
